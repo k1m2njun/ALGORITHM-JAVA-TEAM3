280fa9fd951d90fdd3d4922698317b8e0fb81947
k1m2njun <k1m2njun@naver.com>
2023-04-26 17:39:11 +0900

    BOJ 2979

---
+++ k1m2njun/src/week01/BOJ2979_repair.java
@@ -0,0 +1,39 @@
+package week01;
+
+import java.util.Scanner;
+
+public class BOJ2979_repair {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // 주차요금
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        int[] countArr = new int[100];
+        int total = 0;
+
+        for(int i = 0; i < 3; i++) {
+            int startMinute = scanner.nextInt();
+            int endMinute = scanner.nextInt();
+
+            for(int j = startMinute; j < endMinute; j++) {
+            	countArr[j] ++;
+    		}
+
+        }
+        scanner.close();
+        
+        for(int i = 0; i < 100; i++) {
+        	total += (countArr[i] == 1) ? countArr[i] * A : 0;
+        	total += (countArr[i] == 2) ? countArr[i] * B : 0;
+        	total += (countArr[i] == 3) ? countArr[i] * C : 0;
+		}
+
+        System.out.println(total);
+
+    }
+}
