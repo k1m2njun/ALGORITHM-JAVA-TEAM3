6c1f4a553e5a54cd597f6a0408a65e4b2f9b575d
k1m2njun <k1m2njun@naver.com>
2023-06-05 13:14:46 +0900

    BOJ 11399, 2503

--- k1m2njun/src/week07/BOJ11399.java
+++ k1m2njun/src/week07/BOJ11399.java
@@ -1,12 +1,54 @@
 package week07;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BOJ11399 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+//		Scanner scanner = new Scanner(System.in);
+//		int N = scanner.nextInt();
+//		
+//		int[] arr = new int[N];
+//		
+//		for(int i = 0; i < N; i++) {
+//			arr[i] = scanner.nextInt();
+//		}
+//		Arrays.sort(arr);
+//		
+//		int total = 0;
+//		for(int i = 0; i < arr.length; i++) {
+//			int sum = 0;
+//			for(int j = 0; j < i+1; j++) {
+//				sum = sum + arr[j];
+//			}
+//			total += sum; // 1, 1+2,  
+//		}
+//		System.out.println(total);
+			
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[] list = new int[N];
+		for(int i = 0; i < N; i++) {
+			list[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(list);
+		
+		int sum = 0;
+		int answer = 0;
+		for(int i : list) {
+			sum = (sum + i);
+			answer += sum;
+		}
+		System.out.println(answer);
 	}
-
 }
 
 //https://www.acmicpc.net/problem/11399
+// 시간이 적게 걸리는 사람 순으로 돈을 뽑으면 최소 시간으로 할수 있다.
+
+// 1 + 1+2 + 1+2+3 + 1+2+3+3 + 1+2+3+3+4
+// 1 + 3 + 6 + 9 + 13
+// 32
--- k1m2njun/src/week07/BOJ2503.java
+++ k1m2njun/src/week07/BOJ2503.java
@@ -1,11 +1,108 @@
 package week07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class BOJ2503 {
+	
+	static int N;
+	static List<BaseBallData> inputData = new ArrayList<>();
+	static int MIN_NUMBER = 123;
+	static int MAX_NUMBER = 987;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sc = br.readLine();
+		
+		N = Integer.parseInt(sc);
+		
+		// 입력 데이터 수집
+		for(int i = 0; i < N; i++) {
+			sc = br.readLine();
+			StringTokenizer st = new StringTokenizer(sc, " ");
+			
+            inputData.add(
+            		new BaseBallData(
+            			st.nextToken(), 
+            			Integer.parseInt(st.nextToken()),
+            			Integer.parseInt(st.nextToken())));
+		}
+		
+		System.out.println(calBaseBall());
+		
 	}
+	
+	static int calBaseBall() {
+        int result = 0;
+
+        // 범위 내 모든 수에 베이스볼 게임을 진행.
+        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+            if (!checkNum(i)) continue; // 중복수 및 0 체크해서 제외
+
+            int allTestPass = 0;
+            // 328
+            for (int j = 0; j < N; j++) {
+                int strikeCount = 0; // 1, 1, 2, 0
+                int ballCount = 0;   // 1, 0, 0, 1
+
+                BaseBallData current = inputData.get(j);
+                String currentDataString = current.data; // 민혁이가 영수에게 물은 수
+                String myDataString = Integer.toString(i);
+
+                // 숫자 야구 게임 룰.
+                // String 3개 수를 비교, 만약 i 로 들어온 수가 답이라면 입력받은 s, b의 개수와 동일하게 측정될 것.
+                for (int k = 0; k < 3; k++) {
+                	if (currentDataString.charAt(k) == myDataString.charAt(k)) { // strike
+                        strikeCount++;
+                        continue;
+                    }
+                    for (int u = 0; u < 3; u++) { // ball
+                        if (myDataString.charAt(k) == currentDataString.charAt(u)) { // 수는 같은데
+                            if (k != u) ballCount++; // 위치가 같지 않은 경우 ball 체크
+                        }
+                    }
+                }
+                // strike와 ball의 개수가 입력받은 것과 같으면 카운트 최대 N개
+                if (current.strike == strikeCount && current.ball == ballCount) allTestPass++;
+            }
+            // 어떠한 수에 대해 위의 카운트와 N이 일치하면 카운트 == 베이스볼 게임의 정답으로 예측되는 수임
+            if (allTestPass == N) result++;
+        }
+        return result;
+    }
+	
+	// 중복수 및 0 체크, 없으면 true
+	static boolean checkNum(int num) {
+
+        String numString = Integer.toString(num);
+
+        if ((numString.charAt(0) == numString.charAt(1)) ||
+            (numString.charAt(1) == numString.charAt(2)) ||
+            (numString.charAt(0) == numString.charAt(2))) return false;
+
+        if (numString.charAt(0) == '0' ||
+        	numString.charAt(1) == '0' ||
+        	numString.charAt(2) == '0') return false;
+
+        return true;
+    }
+	
+	static class BaseBallData {
+        String data;
+        int strike;
+        int ball;
+
+        public BaseBallData(String data, int strike, int ball) {
+            this.data = data;
+            this.strike = strike;
+            this.ball = ball;
+        }
+    }
 
 }
 
