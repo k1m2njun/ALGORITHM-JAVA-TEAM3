877598e727cb08b4a64504c89f6d72292f795639
k1m2njun <k1m2njun@naver.com>
2023-06-15 01:00:50 +0900

    PROGRAMMERS 150369, 81301

--- k1m2njun/src/week08/Programmers150369.java
+++ k1m2njun/src/week08/Programmers150369.java
@@ -1,9 +1,41 @@
 package week08;
 
 public class Programmers150369 {
+	
+	public static long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        
+        int delivery = 0;
+        int pickup = 0;
+        
+        // cap < deliveries, pickups X
+        
+        // 먼 집부터 방문
+        for (int i = n - 1; i >= 0; i--) {
+        	delivery += deliveries[i];
+        	pickup += pickups[i];
+        	
+        	while(delivery > 0 || pickup > 0) {
+        		delivery -= cap;
+        		pickup -= cap;
+        		answer += (i+1) * 2;
+        	}
+        }
+        
+        
+        
+        return answer;
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int cap = 2;
+		int n = 7;
+		int[] deliveries = {1, 0, 2, 0, 1, 0, 2};
+		int[] pickups = {0, 2, 0, 1, 0, 2, 0};
+		
+		System.out.println(solution(cap, n, deliveries, pickups));
+		
 
 	}
 
--- k1m2njun/src/week08/Programmers81301.java
+++ k1m2njun/src/week08/Programmers81301.java
@@ -1,11 +1,27 @@
 package week08;
 
 public class Programmers81301 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s = "one4seveneight";
+		
+		System.out.println(solution(s));
+		
 	}
+
+	public static int solution(String s) {
+        int answer = 0;
+        
+        s = s.replace("one", "1").replace("two", "2")
+        		.replace("three", "3").replace("four", "4")
+        		.replace("five", "5").replace("six", "6")
+        		.replace("seven", "7").replace("eight", "9")
+        		.replace("nine", "9").replace("zero", "0");
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
 
 }
 
