dcf815bd347b0c5683ad559fafac0f005d167c52
k1m2njun <k1m2njun@naver.com>
2023-07-05 16:24:49 +0900

    BOJ 2178, 1012

--- k1m2njun/src/week10/BOJ1012.java
+++ k1m2njun/src/week10/BOJ1012.java
@@ -1,10 +1,65 @@
 package week10;
 
+import java.util.Scanner;
+
 public class BOJ1012 {
+	static int[] x = {0, 0, -1, 1};
+	static int[] y = {-1, 1, 0, 0};
+	static boolean[][] visit;
+	static int[][] arr;
+	
+	static int nowX, nowY;
+	static int M, N;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt(); // 테스트 케이스 개수
+		
+		for(int t = 0; t < T; t++) {
+			M = sc.nextInt(); // 배추밭 가로
+			N = sc.nextInt(); // 배추밭 세로
+			int K = sc.nextInt(); // 배추가 심어진 위치 개수
+			
+			arr = new int[N][M];
+			visit = new boolean[N][M];
+			
+			for(int k = 0; k < K; k++) { // 배추 심기
+				int X = sc.nextInt();
+				int Y = sc.nextInt();
+				arr[Y][X] = 1;
+			}
+			
+			int count = 0;
+			for(int n = 0; n < N; n++) {
+				for(int m = 0; m < M; m++) {
+					if(arr[n][m] == 1 && visit[n][m] == false) {
+						count ++;
+						DFS(m, n);
+					}
+				}
+			}
+			System.out.println(count);
+		}
+	}
+	
+	public static void DFS(int X, int Y) {
+		visit[Y][X] = true;
+		
+		for(int i = 0; i < 4; i++) {
+			nowX = X + x[i];
+			nowY = Y + y[i];
+			
+			if(Range_check() 
+					&& visit[nowY][nowX] == false 
+					&& arr[nowY][nowX] == 1) {
+				DFS(nowX, nowY);
+			}
+		}
+	}
+	
+	static boolean Range_check() {
+		return (nowY < N && nowY >= 0 && nowX < M && nowX >= 0);
 	}
 
 }
--- k1m2njun/src/week10/BOJ2178.java
+++ k1m2njun/src/week10/BOJ2178.java
@@ -1,10 +1,61 @@
 package week10;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class BOJ2178 {
+	static int[] x = {0, 0, -1, 1};
+	static int[] y = {-1, 1, 0, 0};
+	static boolean[][] visit;
+	static int[][] maze;
+	
+	static int nowX, nowY;
+	static int M, N;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		maze = new int[N][M];
+		visit = new boolean[N][M];
+		visit[0][0] = true;
+		
+		String row;
+	
+		for(int n = 0; n < N; n++) {
+			row = sc.next();
+			for(int m = 0; m < M; m++) {
+				maze[n][m] = row.charAt(m) - '0';
+			}
+		}
+		
+		bfs(0, 0);
+		System.out.println(maze[N-1][M-1]);
+	}
+	
+	public static void bfs(int X, int Y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {X, Y});
+		
+		while(!queue.isEmpty()) {
+			int now[] = queue.poll();
+			nowX = now[0];
+			nowY = now[1];
+			
+			for(int i = 0; i < 4; i++) {
+				int nextX = nowX + x[i];
+				int nextY = nowY + y[i];
+				
+				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+				if (visit[nextX][nextY] || maze[nextX][nextY] == 0) continue;
+				
+				queue.add(new int[] {nextX, nextY});
+				maze[nextX][nextY] = maze[nowX][nowY] + 1;
+				visit[nextX][nextY] = true;
+			}
+		}
 	}
 
 }
